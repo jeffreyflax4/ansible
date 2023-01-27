1ad9aeed412cbd4ddac808ce29e6bc8af37de964
Jeff Flax <jeff.flax@augustschell.com>
2023-01-27 19:44:08 +0000

    Updated yum_installs.sh

--- yum_installs.sh
+++ yum_installs.sh
@@ -2,8 +2,4 @@
 amazon-linux-extras install epel -y
 yum install ansible -y
 pip3 install ansible
-#yum install python-boto -y
-#yum install -y pip
-#pip install boto boto3
-#pip install --upgrade requests==2.20.1
-#pip3 install boto boto3 botocore
+pip3 install boto3 --user
